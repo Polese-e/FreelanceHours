41f94cb615cd2fed3104e909f8013c6ccabd6956
Polese <enzopolesedamasceno@gmail.com>
2024-10-10 12:04:42 -0300

    Create Livewire Components

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,5 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(200)->create();
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(function (User $user) {
+                $project = Project::factory()->create(['created_by' => $user->id]);
+                Proposal::factory()->count(random_int(5,45))->create(['project_id' => $project->id]);
+            });
     }
 }
